403e5596b3d6f47686de1a26654d14f49ebc3fa2
Ayush Mukhopadhyay <52779201+ayushmukho@users.noreply.github.com>
2023-12-31 21:54:22 +0530

    Added the pdf renderee for both full and half screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 
-import "react-loading-skeleton/dist/skeleton.css";
 import { Toaster } from "@/components/ui/toaster";
+
+import "react-loading-skeleton/dist/skeleton.css";
+import "simplebar-react/dist/simplebar.min.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
